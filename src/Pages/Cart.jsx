bc7293ae78af45cf15d578a168949e17f9c0d4f9
Khryz-Navarro <s.konavarro@usm.edu.ph>
2025-04-20 02:37:45 +0800

    Update Cart.jsx

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router";
+
 //TODO: create cart page
 function Cart() {
   return (
     <>
-      <div className=" justify-center justify-items-center text-center items-center">
-        <h1 className="font-GochiHand">Theres Nothing in Cart yet</h1>
+      <div className="font-GochiHand justify-center justify-items-center text-center items-center text-4xl cursor-progress h-screen w-full p-20">
+        <Link to={"/"}>
+          <h1>Go back</h1>
+        </Link>
+        <div className="p-20 my-20">
+          <h1 className="font-GochiHand justify-center justify-items-center text-center items-center">
+            Theres Nothing in Cart yet
+          </h1>
+        </div>
       </div>
     </>
   );
